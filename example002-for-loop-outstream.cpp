71b6e4738f2cc59491aeedf4c2aa000446a1acf5
m-aryanpour <masoud.aryanpour@gmail.com>
2021-06-22 19:03:50 +0430

    Add files via upload
    example002: shows how to implement a for-loop and write out some result to the console.

---
+++ example002-for-loop-outstream.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+
+int main(int argc, char *argv[])
+{
+	cout << " example of a for loop" << endl;
+	for (int i=0; i<10; i++){
+	   cout<< i << endl; //	sprintf("%d \n",i);
+	}
+
+	return 0;
+}
