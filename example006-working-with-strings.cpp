bfb43aacd8a773c7ef5b30861b50f63e1f36800f
m-aryanpour <masoud.aryanpour@gmail.com>
2021-06-22 21:13:53 +0430

    Update example006-working-with-strings.cpp

--- example006-working-with-strings.cpp
+++ example006-working-with-strings.cpp
@@ -9,16 +9,15 @@ int main(int argc, char *argv[])
 	string message(" enter a sentence: ");
 	string readline, message2=" You entered: ";
 	
-	cout << message;
-	getline (cin, readline)	;
-
-	cout << message2 << readline << endl;
-	cout << " with "<< readline.length() << " characters: ";
-	string separator(readline.length(),'.');
+	cout << message;                  // send a string out
+	getline (cin, readline)	;         // read string from input stream
+	message2 += readline;             // add two strings
+	cout << message2 << endl;
+	cout << " with "<< readline.length() << " characters: "; // length of a string
+	string separator(readline.length(),'.'); // define a long single-character string
 	cout << separator << endl;
 
 
 	return 0;
 }
-
 
