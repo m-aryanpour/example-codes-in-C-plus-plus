f2b3747d64b2c038abb88f1f2f84b948c72fb973
m-aryanpour <masoud.aryanpour@gmail.com>
2021-06-24 10:44:20 +0430

    Update example016-pointer-of-variable.cpp

--- example016-pointer-of-variable.cpp
+++ example016-pointer-of-variable.cpp
@@ -9,6 +9,8 @@ int main()
 	cout << " x-value= " << x << ", x-address= x-pointer= " << &xptr<< endl;
 	cout << " xptr   = " << xptr << ", xptr-address= "<< &xptr << endl;
 	cout << " xptr-pointing-to= " << *xptr<< endl;
+	*xptr += 10;
+	cout << " change x by changing its pointer: <*xptr += 10;> adds 10 to x: "<< x<<endl;
 
 return 0;
 }
